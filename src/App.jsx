04e63d152191322a5dfadd9c33394e0ba5e39f1e
rishi_roushan <rishiroushanhacker@gmail.com>
2025-04-06 22:08:16 +0530

    Normal counter in react js

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+      <p>
+        count is {count}
+      </p>
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          + 
+        </button>
+        <button onClick={() => setCount((count) => count - 1)}>
+          - 
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
